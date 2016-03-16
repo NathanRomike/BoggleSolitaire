bd096bc0171ea4bb2b331393be497cfd1bd0bac3
Illia Kuchko and Nathan Romike <ikuchko@gmail.com>
2016-03-16 09:26:11 -0700

    add initial files

---
+++ app/src/main/java/com/example/guest/bogglesolitaire/GameBoardActivity.java
@@ -0,0 +1,17 @@
+package com.example.guest.bogglesolitaire;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_board);
+
+        Resources res = new Resources();
+        String[] alphabet =
+    }
+}
