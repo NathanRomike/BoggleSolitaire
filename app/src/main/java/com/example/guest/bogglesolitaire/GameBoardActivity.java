bfb0bad76cb623a472390abb2c02d23036e4e588
Illia Kuchko and Nathan Romike <ikuchko@gmail.com>
2016-03-16 16:53:10 -0700

    add word to listView

--- app/src/main/java/com/example/guest/bogglesolitaire/GameBoardActivity.java
+++ app/src/main/java/com/example/guest/bogglesolitaire/GameBoardActivity.java
@@ -1,29 +1,41 @@
 package com.example.guest.bogglesolitaire;
 
 import android.content.res.Resources;
+import android.support.v4.content.res.TypedArrayUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
+
 public class GameBoardActivity extends AppCompatActivity {
     public static final String TAG = GameBoardActivity.class.getSimpleName();
-    @Bind(R.id.Cell0) TextView cell0;
-    @Bind(R.id.Cell1) TextView cell1;
-    @Bind(R.id.Cell2) TextView cell2;
-    @Bind(R.id.Cell3) TextView cell3;
-    @Bind(R.id.Cell4) TextView cell4;
-    @Bind(R.id.Cell5) TextView cell5;
-    @Bind(R.id.Cell6) TextView cell6;
-    @Bind(R.id.Cell7) TextView cell7;
+    private ArrayList<String> resultList = new ArrayList<String>();
+    private ArrayList<String> arrayForAdapter = new ArrayList<String>();
+    private String[] boggleSequence = new String[8];
+    @Bind(R.id.gridview) GridView gridview;
+    @Bind(R.id.wordText) EditText wordText;
+    @Bind(R.id.submitButton) Button submitButton;
+    @Bind(R.id.resultListView) ListView resultListView;
 
+    ArrayAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +45,7 @@ public class GameBoardActivity extends AppCompatActivity {
 
         String[] alphabet = this.getResources().getStringArray(R.array.alphabet);
         String[] vowels = this.getResources().getStringArray(R.array.vowels);
-        String[] boggleSequence = new String[8];
+
 
 
         Boolean isTwoVowels = false;
@@ -43,7 +55,46 @@ public class GameBoardActivity extends AppCompatActivity {
             Log.d(TAG, Boolean.toString(validateVowels(boggleSequence, vowels)));
         } while (!validateVowels(boggleSequence, vowels));
 
-        cell0.setText(boggleSequence[0]);
+        arrayForAdapter = new ArrayList<String>(Arrays.asList(boggleSequence));
+
+        mAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayForAdapter);
+        gridview.setAdapter(mAdapter);
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(GameBoardActivity.this, parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+                if (!parent.getItemAtPosition(position).toString().equals("")){
+                    wordText.append(parent.getItemAtPosition(position).toString());
+                    String letter = parent.getItemAtPosition(position).toString();
+                    mAdapter.remove(letter);
+                    mAdapter.insert("", position);
+                    mAdapter.notifyDataSetChanged();
+                }
+            }
+        });
+//
+        wordText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wordText.setText("");
+            }
+        });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resultList.add(wordText.getText().toString());
+                ArrayAdapter adapterList = new ArrayAdapter(GameBoardActivity.this, android.R.layout.simple_list_item_1, resultList);
+                resultListView.setAdapter(adapterList);
+                wordText.setText("");
+                mAdapter.clear();
+                mAdapter.addAll(boggleSequence);
+//                mAdapter = new ArrayAdapter(GameBoardActivity.this, android.R.layout.simple_list_item_1, arrayForAdapter);
+//                gridview.setAdapter(mAdapter);
+                mAdapter.notifyDataSetChanged();
+            }
+        });
     }
 
     private String[] randomSequence (String[] alphabet, int amount) {
