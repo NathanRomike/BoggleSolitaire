7a4cd29e0e97236085fc16f2ac1a81e5ea34152f
Illia Kuchko and Nathan Romike <nathanromike@gmail.com>
2016-03-16 11:16:43 -0700

    Add methods to create random sequence and method validate there are at least 2 vowels.

--- app/src/main/java/com/example/guest/bogglesolitaire/GameBoardActivity.java
+++ app/src/main/java/com/example/guest/bogglesolitaire/GameBoardActivity.java
@@ -3,15 +3,59 @@ package com.example.guest.bogglesolitaire;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.Random;
 
 public class GameBoardActivity extends AppCompatActivity {
+    public static final String TAG = GameBoardActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_board);
 
-        Resources res = new Resources();
-        String[] alphabet =
+        String[] alphabet = this.getResources().getStringArray(R.array.alphabet);
+        String[] vowels = this.getResources().getStringArray(R.array.vowels);
+        String[] boggleSequence = new String[8];
+
+
+        Boolean isTwoVowels = false;
+        do {
+            boggleSequence = randomSequence(alphabet, 8);
+            Log.d(TAG, "new sequence");
+            Log.d(TAG, Boolean.toString(validateVowels(boggleSequence, vowels)));
+        } while (!validateVowels(boggleSequence, vowels));
+
+//        Toast.makeText(GameBoardActivity.this, alphabet[number], Toast.LENGTH_LONG).show();
+    }
+
+    private String[] randomSequence (String[] alphabet, int amount) {
+        Random random = new Random();
+        String[] sequence = new String[amount];
+        for(int i = 0; i < 8; i++) {
+            int number = random.nextInt(26);
+            sequence[i] = alphabet[number];
+            Log.d(TAG, String.valueOf(sequence[i]));
+        }
+        return sequence;
+    }
+
+    private Boolean validateVowels (String[] sequence, String[] vowels) {
+        int vowelsAmount = 0;
+        for (int i = 0; i < vowels.length; i++) {
+            for (int j = 0; j < sequence.length; j++) {
+                if (sequence[j].equals(vowels[i])) {
+                    vowelsAmount ++;
+                }
+            }
+        }
+        if (vowelsAmount >= 2) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- app/src/main/java/com/example/guest/bogglesolitaire/MainActivity.java
+++ app/src/main/java/com/example/guest/bogglesolitaire/MainActivity.java
@@ -1,13 +1,83 @@
 package com.example.guest.bogglesolitaire;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends ActionBarActivity {
+    @Bind(R.id.startButton) Button startButton;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        ButterKnife.bind(this);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GameBoardActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.guest.bogglesolitaire/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.guest.bogglesolitaire/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
